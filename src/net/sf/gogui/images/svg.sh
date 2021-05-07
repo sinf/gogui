ea4071579b1b54b1bddbc75e37be231adc3b7bd8
ArhoM <arho.iivari@elisanet.fi>
2021-05-07 16:10:01 +0300

    svg.sh: update to work with inkscape 1.0.2
    Also don't rebuild the PNGs that already exist

--- src/net/sf/gogui/images/svg.sh
+++ src/net/sf/gogui/images/svg.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+conv() {
+	out="$(basename $1 .svg)-${2}x${2}.png"
+	echo "$out"
+	if [ ! -f "$out" ]; then
+		inkscape -o "$out" --export-type=png -h "$2" -w "$2" "$1"
+	fi
+}
+
 for i in 16 24 32 48 64
 do
 	for file in *.svg
 	do
 		echo "conversion svg> pgn de " $file
-		inkscape --file="$file" --export-png=$(basename $file .svg)"-${i}x${i}.png" --export-width=$i
+		conv "$file" $i
 	done
 done
 
 for file in "gogui-black.svg" "gogui-white.svg" "gogui-setup.svg"
 do
-	inkscape --file="$file" --export-png=$(basename $file .svg)"-8x8.png" --export-width=8
+	conv "$file" 8
 done
