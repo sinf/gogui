ee6d25b0d70fa808e74116a3fbf5b0cfae79ddc5
ArhoM <arho.iivari@elisanet.fi>
2021-05-07 16:06:11 +0300

    ubuntu_setup.sh: dont use apt if not available
    Also don't redownload quaqua if running this script twice.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
+set -x
 
-sudo apt install ant xsltproc docbook-xsl inkscape
+if which apt; then
+	sudo apt install ant xsltproc docbook-xsl inkscape
+fi
 
-cd /tmp
-wget https://www.randelshofer.ch/quaqua/files/quaqua-8.0.nested.zip
-unzip quaqua-8.0.nested.zip
-unzip quaqua-8.0.zip
-cd -
+if [ ! -f /tmp/Quaqua/dist/quaqua.jar ]; then
+	cd /tmp
+	wget https://www.randelshofer.ch/quaqua/files/quaqua-8.0.nested.zip
+	unzip quaqua-8.0.nested.zip
+	unzip quaqua-8.0.zip
+	cd -
+fi
 
 mkdir -p lib
 cd lib
