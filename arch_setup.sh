f602ab70987d639ef252ea7e2617ca2f07305061
ArhoM <arho@amahl.fi>
2022-04-26 14:46:13 +0300

    arch_setup.sh: setup script for Arch Linux

---
+++ arch_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -x
+
+#sudo apt install ant xsltproc docbook-xsl inkscape
+
+if [ ! -f /tmp/Quaqua/dist/quaqua.jar ]; then
+	cd /tmp
+	wget https://www.randelshofer.ch/quaqua/files/quaqua-8.0.nested.zip
+	unzip quaqua-8.0.nested.zip
+	unzip quaqua-8.0.zip
+	cd -
+fi
+
+mkdir -p lib
+cd lib
+cp /tmp/Quaqua/dist/quaqua.jar .
+cd -
+
+cd src/net/sf/gogui/images
+./svg.sh
+cd -
+
+ant
